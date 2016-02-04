fcfade06a414072f92cb7a1de6e20bc4a197a5d4
jamesleong <jyleong@sfu.ca>
2016-02-04 03:08:41 +0000

    extract redundany code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update,:show, :destroy]
   def new
     @article = Article.new
   end
   def index
     @articles = Article.all
   end
+  
   def edit
-    @article = Article.find(params[:id])
+    
   end
   def create
     #render plain: params[:article].inspect
@@ -21,7 +23,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -31,11 +33,11 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "article was successfully destroyed"
     redirect_to articles_path
@@ -43,6 +45,10 @@ class ArticlesController < ApplicationController
   
   ## from params hash makes article from these two items
   private
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title,:description)
     end
