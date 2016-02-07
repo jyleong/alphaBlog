9717a3b01f41e915b75773089823b15231009d5b
jamesleong <jyleong@sfu.ca>
2016-02-07 19:51:07 +0000

    categories pagination, list categories int test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,4 +11,18 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_match "sports", response.body
   end
+  
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 0 do
+      ## no change in my category count
+      post categories_path, category: {name: ""}
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+    ## look for tiel and body from errors partials
+    
+  end
 end
