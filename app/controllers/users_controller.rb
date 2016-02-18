6edbc0882840086c3b1976787c4d60306f686065
jamesleong <jyleong@sfu.ca>
2016-02-18 02:59:45 +0000

    added searching for users and friends js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,12 +56,22 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
   
+  def search
+    @users = User.search(params[:search_param])
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial: 'friends/lookup'
+    else
+      render status: :not_found, nothing: true
+    end
+  end
+  
   def add_friend
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
     
     if current_user.save
-      redirect_to my_friends_path, notice: "Friend was successfully added"
+      redirect_to my_friends_path, flash[:successs] = "Friend was successfully added"
     else
       redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,42 @@ class User < ActiveRecord::Base
             format: {with: VALID_EMAIL_REGEX}
   ##regex ceck for email format
   has_secure_password
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1 ## if not show up at least once, doesn't exist
+  end
+  
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id} ## return collection without the current user
+  end
+  
+  def self.search(param)
+    # debugger
+    return User.none if param.blank?
+    
+    param.strip!
+    param.downcase!
+    email_matches(param).uniq
+    ##(first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    ##must define thsese methods, object oriented programming, outsource parts of it
+  end
+  
+  # def self.first_name_matches(param)
+  #   matches('first_name', param)
+  # end
+  
+  # def self.last_name_matches(param)
+  #   matches('last_name',param)
+  # end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+  
+  def self.matches(field_name, param) ## query db
+    where("lower(#{field_name}) like ?", "%#{param}%")
+    ##% is wild card, so doesn't ahve to be exact match
+  end
+  
+  
 end
